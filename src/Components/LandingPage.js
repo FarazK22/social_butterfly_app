f5fd56038dcc147945e8cf279c41c37657d82806
faraz <faraz.kazi22@gmail.com>
2021-04-10 15:43:33 -0700

    landing page format templated

--- src/Components/LandingPage.js
+++ src/Components/LandingPage.js
@@ -1,26 +1,24 @@
 import React, { Component } from "react";
 import "./landingpage.css";
 import { Helmet } from "react-helmet";
-import { Flex, Box } from 'reflexbox'
+import { MDBContainer, MDBRow, MDBCol } from "mdbreact";
 
 class LandingPage extends Component {
   render() {
     return (
+
+      
       <div className = "background">
-        <Flex p={2} align='center'>
-          <Box px={2} height = {1/2}>
-            <Helmet>
+        <MDBContainer>
+          <MDBRow>
+            <MDBCol md="8">
               <link rel="stylesheet" href="landingpage.css" />
-            </Helmet>
-            <h1> landing page </h1>
-          </Box>
-          <Box px={2} height={1/3}>
-              <p> hello world </p>
-          </Box>
-          <Box px={2} height= {1/4}>
-            <p> gelo </p>
-          </Box>
-        </Flex>
+              <h1> landing page </h1>
+            <MDBCol md ="8"> bbb </MDBCol>
+            </MDBCol>
+            <MDBCol md="4">hellooooooooooooooooooooooooooooooooooooooooooooooooooo </MDBCol>
+         </MDBRow>
+        </MDBContainer>   
       </div>
 
     )
