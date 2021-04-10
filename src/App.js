5e20126d610fa5a44726b6b758f753e1a625776b
Ali <ali.hasan3232@gmail.com>
2021-04-10 12:11:07 -0500

    the structure

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
-import Counter from './Components/Counter'
+import SignUp from './Components/SignUp'
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
-      <Counter></Counter>
+      <SignUp></SignUp>
     </div>
   );
 }
