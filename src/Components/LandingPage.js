cdf2cfbbade38f921609ecadd6422e6cebeb46c6
Ali <ali.hasan3232@gmail.com>
2021-04-10 14:26:57 -0500

    landingpage css in works

--- src/Components/LandingPage.js
+++ src/Components/LandingPage.js
@@ -1,11 +1,17 @@
 import React, { Component } from "react";
-//import './LandingPage.css';
+import "./landingpage.css";
+import { Helmet } from "react-helmet";
 
 class LandingPage extends Component {
   render() {
     return (
-
-      <p> hello world </p>
+      <div className="login container">
+        <Helmet>
+          <link rel="stylesheet" href="landingpage.css" />
+        </Helmet>
+        <h1> landing page </h1>
+        <p> hello world </p>
+      </div>
 
 
     )
