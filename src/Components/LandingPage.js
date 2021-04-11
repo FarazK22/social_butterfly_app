e613569878b3a6f4187ae6a0199ae25a3f378415
faraz <faraz.kazi22@gmail.com>
2021-04-10 18:00:31 -0700

    profile pic added, layout created for landing page

--- src/Components/LandingPage.js
+++ src/Components/LandingPage.js
@@ -1,25 +1,42 @@
 import React, { Component } from "react";
 import "./landingpage.css";
-import { Helmet } from "react-helmet";
-import { MDBContainer, MDBRow, MDBCol } from "mdbreact";
+import Row from 'react-bootstrap/Row'
+import Container from 'react-bootstrap/Container'
+import Col from 'react-bootstrap/Col'
+import Image from 'react-bootstrap/Image'
+import faraz from "./fk1.jpg";
+
+
 
 class LandingPage extends Component {
   render() {
     return (
-
-      
       <div className = "background">
-        <MDBContainer>
-          <MDBRow>
-            <MDBCol md="8">
-              <link rel="stylesheet" href="landingpage.css" />
-              <h1> landing page </h1>
-            <MDBCol md ="8"> bbb </MDBCol>
-            </MDBCol>
-            <MDBCol md="4">hellooooooooooooooooooooooooooooooooooooooooooooooooooo </MDBCol>
-         </MDBRow>
-        </MDBContainer>   
-      </div>
+       <Container>
+  <Row>
+    <div className = "profilebanner">
+    <Col>
+      <div className = 'profilepic'>
+                <Image src={faraz} thumbnail/>
+            </div>
+    </Col>
+    </div>
+    <div className = "friendProfile1">
+    <Col>Friend Profile 1</Col>
+    </div>
+  </Row>
+  <Row>
+    <div className = "profileinfo">
+      <Col>
+        Profile info
+      </Col>
+    </div>
+    <div className = "friendProfile2">
+    <Col>Friend Profile 2</Col>
+    </div>
+  </Row>
+</Container>
+      </div>   
 
     )
   }
